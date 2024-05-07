860c4233c26cf4ae940fbb1aae0de285b65b69c6
Abrar <hossainabrar@aol.com>
2024-05-06 21:24:54 -0400

    partially completed, need to complete last test

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value='', is_sentence=False):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  @value.setter
+  def value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+
+  def is_sentence(self):
+    return self.value[-1] =='.'
+
+  def is_question(self):
+    return self.value[-1] =='?'
+
+  def is_exclamation(self):
+    return self.value[-1] =='!'
+
+  def count_sentences(self):
+    count = self.value.count('.')
+    count += self.value.count('?')
+    count += self.value.count('!')
+    return count
